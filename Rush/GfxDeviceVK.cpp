e296237b431a9aeb420395b8c4394d0b709fc000
Yuriy O'Donnell <yuriyo@gmail.com>
2018-03-10 15:35:37 +0100

    Validation error fix

--- Rush/GfxDeviceVK.cpp
+++ Rush/GfxDeviceVK.cpp
@@ -1965,7 +1965,7 @@ void GfxContext::addImageBarrier(VkImage image, VkImageLayout nextLayout, VkImag
 	if (m_type == GfxContextType::Transfer)
 	{
 		srcStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT;
-		dstStageMask = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+		dstStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT;
 	}
 
 	m_pendingBarriers.srcStageMask |= srcStageMask;
