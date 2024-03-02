c8d63f9133f296e7d6396f25ae6afbbc46d0e462
Yuriy O'Donnell <yuriyo@gmail.com>
2024-03-02 12:21:23 -0800

    Add basic UniquePtr
    Tweak UniquePtr

--- Rush/UtilMemory.h
+++ Rush/UtilMemory.h
@@ -13,4 +13,37 @@ inline void deallocateBytes(void* ptr)
 	delete [] (char*)ptr;
 }
 
+template <typename T> class UniquePtr
+{
+public:
+	UniquePtr() = default;
+	explicit UniquePtr(T* in_p) : p(in_p) {}
+	UniquePtr(nullptr_t*) : p(nullptr) {}
+	UniquePtr(const UniquePtr&)            = delete;
+	UniquePtr(UniquePtr&& other) : p(other.p)
+	{
+		other.p = nullptr;
+	}
+	UniquePtr& operator=(const UniquePtr&) = delete;
+	UniquePtr& operator=(UniquePtr&& other)
+	{
+		if (p != other.p)
+		{
+			delete p;
+			p = other.p;
+			other.p = nullptr;
+		}
+		return *this;
+	}
+	~UniquePtr() { delete p; }
+
+	T*       operator->() { return p; }
+	const T* operator->() const { return p; }
+	T*       get() { return p; }
+	const T* get() const { return p; }
+
+private:
+	T* p = nullptr;
+};
+
 }
